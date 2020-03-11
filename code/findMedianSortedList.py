9bd37b53b7ec33a390282ec1cdb03a3fdf13ed0c
JoltShining <shinjolt@gmail.com>
2020-03-11 15:55:02 +0800

    update a hard method

--- code/findMedianSortedList.py
+++ code/findMedianSortedList.py
@@ -9,12 +9,26 @@ Describe:algo example leetcode find two sort list median and O(log(m+n)),m,n is 
 class FindMedianSortedList:
 
     def find_median_two_sorted_list(self,listA,listB):
+        """
+        有bug,需改进,暂时未改进
+        :param listA:
+        :param listB:
+        :return:
+        """
+        if not listA and not listB:return []
         #  list A的长度小于等于 list B的长度
         if len(listA) > len(listB):
             listA, listB = listB, listA
         lengthA,lengthB = len(listA),len(listB)
+        if lengthA == 0:
+            if lengthB % 2 == 0:
+                return (listB[lengthB // 2 - 1] + listB[lengthB // 2]) / 2
+            else:
+                return listB[lengthB // 2]
+
         length = lengthA + lengthB
         medium = length // 2
+        print("medium:%s"%medium)
         # listA 最大值小于listB的最小值，取值在A的最后一个元素或全部在B中
         if listA[lengthA - 1] <= listB[0]:
             if length % 2 == 0:
@@ -36,25 +50,43 @@ class FindMedianSortedList:
         low = 0
         high = lengthA
         # 取list A 和list B的中间的值
-        while low <= high:
+        while low < high:
             posA = (low + high) // 2
-            posB = (lengthA + lengthB +1) // 2 - posA
+            posB = (lengthA + lengthB) // 2 - posA
 
-            leftA = 0 if posA == 0 else listA[posA-1]
-            leftB = 0 if posB == 0 else listB[posB-1]
+            leftA = listA[0] if posA == 0 else listA[posA-1]
+            leftB = listB[0] if posB == 0 else listB[posB-1]
 
-            rightA = 0 if posA == 0 else listA[posA]
-            rightB = 0 if posB == 0 else listB[posB]
+            rightA = listA[0] if posA == 0 or posA == lengthA else listA[posA]
+            rightB = listB[0] if posB == 0 else listB[posB]
 
             if leftA <= rightB and leftB <= rightA:
                 if (lengthA + lengthB) % 2 == 0:
                     return (max(leftA,leftB) + min(rightA,rightB)) / 2
+                elif rightA >= rightB:
+                    return max(leftA,rightB)
                 else:
                     return max(leftA,leftB)
             elif rightA < leftB:
                 low = posA + 1
             else:
                 high = posA - 1
+
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        执行用时 :56 ms, 在所有 Python3 提交中击败了90.34%的用户
+        内存消耗 :13.6 MB, 在所有 Python3 提交中击败了9.24%的用户
+        :param nums1:
+        :param nums2:
+        :return:
+        """
+        temp = nums1 + nums2
+        temp.sort()
+        length = len(temp)
+        if length % 2 == 0:
+            return (temp[length//2 - 1] + temp[length//2])/2
+        else:
+            return temp[length//2]
 
 
 if __name__ == "__main__":
@@ -65,7 +97,16 @@ if __name__ == "__main__":
     listD = [4,4,8]
     listF = [1,3,29]
     listE = [4,9,10,25]
+    nums1 = [1, 3]
+    nums2 = [2]
+    nums3 = [1,2]
+    nums4 = [-1,3]
+    nums5 = [2]
+    nums6 = [1,3,4]
     findmedian = FindMedianSortedList()
-    print(findmedian.find_median_two_sorted_list(listA,listB))
-    print(findmedian.find_median_two_sorted_list(listC, listD))
-    print(findmedian.find_median_two_sorted_list(listE, listF))
+    # print(findmedian.find_median_two_sorted_list(listA,listB))
+    # print(findmedian.find_median_two_sorted_list(listC, listD))
+    # print(findmedian.find_median_two_sorted_list(listE, listF))
+    # print(findmedian.find_median_two_sorted_list(nums1,nums2))
+
+    print(findmedian.find_median_two_sorted_list(nums5, nums6))
